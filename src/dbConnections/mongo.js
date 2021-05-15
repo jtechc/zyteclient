6db2cfb7aa4174b78dbf1bacd79594697f8aae61
Zack <zytesas@Zack-Main.localdomain>
2021-05-15 10:54:17 -0400

    fixed mongo error

--- src/dbConnections/mongo.js
+++ src/dbConnections/mongo.js
@@ -2,7 +2,6 @@ const { connect } = require('mongoose');
 const mongoURI = process.env.MONGO_URI;
 const colors = require('colors');
 
-/** @param {string} mongoURI */
 module.exports = async () => {
     await connect(mongoURI, {
     useNewUrlParser: true,
